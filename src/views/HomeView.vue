de67b6208ca3cb6180d0e823ccba646fefdaabd8
xsalefter <xsalefter@gmail.com>
2023-09-11 23:42:11 +0700

    add dumb method that return vue-aviate-components and aviate-client-js classes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import type { HeaderDeploymentsItem } from 'vue-aviate-components';
+import * as aviate from 'aviate-client-js';
+
+function createDeploymentItem(): HeaderDeploymentsItem {
+  return null;
+}
+
+function createQuoteApi(): aviate.QuoteApi {
+  return null;
+}
 </script>
 
 <template>
